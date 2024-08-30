9aa4a105cf635997e0dd312f1029a65cd07c4aee
Scott Andrew Bedis <bedisscottandrew@gmail.com>
2024-08-30 20:31:01 +0800

    chore: move hero section to page  and change name of the component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
-import Link from 'next/link'
+import HeroSection from '@/components/ui/hero-section/HeroSection';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex-grow">
+    <div className='flex-grow'>
+      <HeroSection />
       Home
-
-      <nav className="navbar">
-        <ul className="nav-list">
-          <li className="nav-item"><Link href="/login">Login</Link></li>
-          <li className="nav-item"><Link href="/register">Register</Link></li>
-          <li className="nav-item"><Link href="/about-us">About Us</Link></li>
-          <li className="nav-item"><Link href="/auction">Auction</Link></li>
-          <li className="nav-item"><Link href="/manila-fame">Manila Fame</Link></li>
-          <li className="nav-item"><Link href="/spotlight">Spotlight</Link></li>
-          <li className="nav-item"><Link href="/stories">Stories</Link></li>
+      <nav className='navbar'>
+        <ul className='nav-list'>
+          <li className='nav-item'>
+            <Link href='/login'>Login</Link>
+          </li>
+          <li className='nav-item'>
+            <Link href='/register'>Register</Link>
+          </li>
+          <li className='nav-item'>
+            <Link href='/about-us'>About Us</Link>
+          </li>
+          <li className='nav-item'>
+            <Link href='/auction'>Auction</Link>
+          </li>
+          <li className='nav-item'>
+            <Link href='/manila-fame'>Manila Fame</Link>
+          </li>
+          <li className='nav-item'>
+            <Link href='/spotlight'>Spotlight</Link>
+          </li>
+          <li className='nav-item'>
+            <Link href='/stories'>Stories</Link>
+          </li>
         </ul>
       </nav>
-
     </div>
   );
 }
