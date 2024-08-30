1a61f5c9f3bd9fb3576b8701b213ea4639435842
joemar25 <joemar.cardino000@gmail.com>
2024-08-30 17:39:39 +0800

    Refactor layout and footer components
    - Refactor layout component to include a flex container for children elements
    - Add new footer component with updated styling and menu items
    - Delete old footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1> Content </h1>;
+  return (
+    <main className="flex-grow">
+      Home
+    </main>
+  );
 }
--- src/components/layouts/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <h1>
-      This is foot loose
-    </h1>
-  );
-};
-
-export default Footer;
