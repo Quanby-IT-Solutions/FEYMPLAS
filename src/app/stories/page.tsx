e997920294fc14177dbd8672c958f7acf860f51a
joemar25 <joemar.cardino000@gmail.com>
2024-09-03 18:31:40 +0800

    Reuse things in stories

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -1,31 +1,29 @@
 import type { NextPage } from "next";
-
+import Image from "next/image";
 import { ImageSlider } from "@/components/ui/stories-touchpoint-section/stories-image-slider";
-// import StoriesCategoryComponent from "@/components/ui/stories-touchpoint-section/stories-categories";
 import YourEmail from "@/components/ui/stories-touchpoint-section/stories-your-email";
-// import Subcribe from "@/components/ui/stories-section/stories-subscribe";
 import Footer from "@/components/layouts/about-us-footer/about-us-footer";
 import StoriesCategoryComponentv2 from "@/components/ui/stories-touchpoint-section/stories-categoriesv2";
-
 
 const Root: NextPage = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center">
-      <section className="w-full py-6 text-timeless-black relative flex justify-center items-center">
-      <img
-          className="w-[500px] object-cover"
+      <section className="w-full py-6 text-timeless-black flex justify-center items-center relative">
+        <Image
+          className="object-cover"
           alt="Titlepoint"
           src="/assets/stories-touchpoint/pointtitle.png"
+          width={500}
+          height={300}
+          quality={75}
         />
-          <div>
-          <button className="p-4 w-32 bg-secondary-1 text-timeless-white font-medium fixed right-0 z-20">
-            FaQ&apos;s
-          </button>
-        </div>
+        <button className="p-4 w-32 bg-secondary-1 text-timeless-white font-medium fixed right-0 z-20">
+          FaQ&apos;s
+        </button>
       </section>
       <ImageSlider />
-      <section className="w-full flex flex-row gap-4 justify-center items-center py-32">
-      <StoriesCategoryComponentv2 />
+      <section className="w-full flex gap-4 justify-center items-center py-32">
+        <StoriesCategoryComponentv2 />
       </section>
       <YourEmail />
       <Footer />
