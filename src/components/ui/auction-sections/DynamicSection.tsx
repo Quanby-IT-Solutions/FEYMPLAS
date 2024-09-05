d8897abb6d894886a873c33aece7798e017bf228
joemar25 <joemar.cardino000@gmail.com>
2024-09-05 23:24:12 +0800

    Refactor LotsThirdSection and AuctionSecondSection components

--- src/components/ui/auction-sections/DynamicSection.tsx
+++ src/components/ui/auction-sections/DynamicSection.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import PropTypes from "prop-types";
-import { motion } from "framer-motion";
-import { useEffect, useState, useRef } from "react";
+import Image from "next/image";
+import { useRef } from "react";
 
 interface Auction {
   id: number;
@@ -13,82 +12,44 @@ interface Auction {
   startingBid?: string;
   venue?: string;
   button?: string;
-  width: number;
-  height: number;
+  inlineSize: number;
+  blockSize: number;
 }
 
 export const DynamicSection = ({ auctions }: { auctions: Auction[] }) => {
-  const [displayedAuctions, setDisplayedAuctions] =
-    useState<Auction[]>(auctions);
-  const [hovered, setHovered] = useState<number | null>(null);
-  const [hoverTimer, setHoverTimer] = useState<NodeJS.Timeout | null>(null);
   const containerRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseEnter = (id: number) => {
-    const timer = setTimeout(() => {
-      setHovered(() => id);
-    }, 500); // Trigger hover effect after 0.5 seconds
-    setHoverTimer(timer);
-  };
-
-  const handleMouseLeave = () => {
-    clearTimeout(hoverTimer!);
-    setHovered(null); // Remove hover effect immediately when the cursor leaves
-  };
-
-  useEffect(() => {
-    return () => {
-      // Cleanup hover timer
-      if (hoverTimer) {
-        clearTimeout(hoverTimer);
-      }
-    };
-  }, [hoverTimer]);
 
   return (
     <div className="w-full">
-      <div
-        className="w-full overflow-x-auto scroll-none h-[100dvh]"
-        ref={containerRef}
-      >
-        <div className="flex space-x-8 py-4 items-end h-full px-6">
-          {displayedAuctions.map((auction) => (
+      <div className="w-full overflow-x-auto scroll-none h-[100dvh] p-4" ref={containerRef}>
+        <div className="flex space-x-8 items-end h-full">
+          {auctions.map((auction, index) => (
             <div
               key={auction.id}
               className="flex-shrink-0 flex flex-col items-center pt-12"
-              style={{ width: `${auction.width}px` }}
+              style={{ inlineSize: `${auction.inlineSize}px` }}
             >
-              <motion.div
-                className="w-full mb-2 overflow-visible"
-                initial={{ height: auction.height, boxShadow: "none" }}
-                animate={
-                  hovered === auction.id
-                    ? {
-                        height: 700,
-                        boxShadow: "0px 10px 30px rgba(0, 0, 0, 0.1)",
-                      }
-                    : { height: auction.height, boxShadow: "none" }
-                }
-                transition={{ duration: 0.3 }}
-                onMouseEnter={() => handleMouseEnter(auction.id)}
-                onMouseLeave={handleMouseLeave}
+              <div
+                className={`w-full mb-4 overflow-hidden shadow-md ${index === 1 ? "max-inline-size-[640px] h-[600px]" : `max-inline-size-[640px] h-[${auction.blockSize}px]`
+                  }`}
               >
-                <motion.img
-                  className="w-full h-full object-cover"
+                <Image
                   src={auction.image}
                   alt={auction.title}
-                  style={{ height: "100%", paddingTop: 50 }}
+                  layout="responsive"
+                  width={auction.inlineSize}
+                  height={auction.blockSize}
+                  className="object-cover"
+                  style={{ maxInlineSize: "100%", blockSize: "100%", paddingBlockStart: "50px" }}
                 />
-              </motion.div>
-              <div className="w-full justify-start items-start text-primary-2">
-                <h3 className="font-semibold text-xl mb-1 text-primary-2">
-                  {auction.title}
-                </h3>
+              </div>
+              <div className="w-full text-primary-2 flex flex-col items-start">
+                <h3 className="font-semibold text-xl mb-1 text-primary-2">{auction.title}</h3>
                 <p className="text-lg mb-2">{auction.venue}</p>
                 <p className="text-lg mb-2">{auction.estimate}</p>
                 <p className="text-lg mb-2">{auction.startingBid}</p>
                 {auction.button && (
-                  <button className="w-40 p-4 border border-[#6a704c] text-[#6a704c] font-semibold hover:bg-[#6a704c] hover:text-white transition-colors">
+                  <button className="w-40 p-4 border border-[#6a704c] text-[#6a704c] font-semibold hover:bg-[#6a704c] hover:text-white transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#6a704c]">
                     {auction.button}
                   </button>
                 )}
@@ -101,19 +62,18 @@ export const DynamicSection = ({ auctions }: { auctions: Auction[] }) => {
   );
 };
 
-// Define PropTypes for validation
 DynamicSection.propTypes = {
   auctions: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number,
-      image: PropTypes.string,
-      title: PropTypes.string,
+      id: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
       venue: PropTypes.string,
       button: PropTypes.string,
-      width: PropTypes.number,
-      height: PropTypes.number,
+      inlineSize: PropTypes.number.isRequired,
+      blockSize: PropTypes.number.isRequired,
       estimate: PropTypes.string,
       startingBid: PropTypes.string,
     })
-  ),
+  ).isRequired,
 };
